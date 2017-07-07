7551b6e82b5094c7f75b3a72cb6ec45e0d6e58b7
Chris Kansas <ckansas@smcm.edu>
2017-07-07 16:04:04 -0600

    Added Imports and TODOs.

--- photoorganizer.py
+++ photoorganizer.py
@@ -2,4 +2,12 @@
 
 #This program will walk through my photo folders and files and organize them by month and year.
 
-import os, shutil
+import os, shutil, pillow
+
+#TODO Move to photo directory if not already there.
+
+#TODO Walk through the photos in each of the folders in the directory.
+
+#TODO Sort photos by creation date (Month/Year)
+
+#TODO Move the photos to the appropriate Month/Year Folders -- Create those folders if necessary.

dd09b03cde423f8611e940ebd864307f6a4d03ad
Chris Kansas <ckansas@smcm.edu>
2017-07-09 07:22:12 -0600

    Tested the changing of the directory - everything works.

--- photoorganizer.py
+++ photoorganizer.py
@@ -2,14 +2,15 @@
 
 #This program will walk through my photo folders and files and organize them by month and year.
 
-import os, shutil, pillow
+import os, shutil, PIL
 
 #TODO Move to photo directory if not already there.
 currentDirectory = os.getcwd()
 
-if currentDirectory != os.path.abspath(r"E:\Pictures\Pictures\Pictures"):
-    os.chdir("E:\Pictures\Pictures\Pictures")
+if currentDirectory != r"E:\Pictures\Pictures\Pictures":
+    os.chdir(r"E:\Pictures\Pictures\Pictures")
     currentDirectory = os.getcwd()
+
 
 #TODO Walk through the photos in each of the folders in the directory.
 

0e1b95a1c3c2caccd2500ed120958207c3be0259
Chris Kansas <ckansas@smcm.edu>
2017-07-09 08:29:52 -0600

    Added walk through functionality, checks against a RegEx to make sure filename ends in .JPG

--- photoorganizer.py
+++ photoorganizer.py
@@ -2,7 +2,10 @@
 
 #This program will walk through my photo folders and files and organize them by month and year.
 
-import os, shutil, PIL
+import os, shutil, PIL, re
+from PIL import Image
+
+photoRegEx = re.compile(r".JPG$")
 
 #TODO Move to photo directory if not already there.
 currentDirectory = os.getcwd()
@@ -11,8 +14,14 @@ if currentDirectory != r"E:\Pictures\Pictures\Pictures":
     os.chdir(r"E:\Pictures\Pictures\Pictures")
     currentDirectory = os.getcwd()
 
-
 #TODO Walk through the photos in each of the folders in the directory.
+for folderName, subfolders, files in os.walk(currentDirectory):
+    print(folderName)
+    for filename in files:
+        if photoRegEx.search(filename) != None:
+            print(filename)
+        #photo_files = Image.open(files)
+        #photo._getexif()[36867]
 
 #TODO Sort photos by creation date (Month/Year)
 
